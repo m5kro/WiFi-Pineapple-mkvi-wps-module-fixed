977785f93a0cd5e2c6473a730fe6fd453a7d318d
kraken5 <44246711+kraken5@users.noreply.github.com>
2022-07-08 00:22:43 -0700

    Add files via upload

---
+++ wps/scripts/dependencies.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+#2022 - m5kro aka kraken5
+
+[[ -f /tmp/wps.progress ]] && {
+  exit 0
+}
+
+touch /tmp/wps.progress
+
+if [ "$1" = "install" ]; then
+  if [ "$2" = "internal" ]; then
+	 opkg update
+
+     opkg install /pineapple/modules/wps/ipks/reaver-custom_1.6.6-5_mips_24kc.ipk
+     opkg install /pineapple/modules/wps/ipks/bully_1.4-1_mips_24kc.ipk
+     opkg install /pineapple/modules/wps/ipks/pixiewps-custom_1.4.2-3_mips_24kc.ipk
+     opkg install libpcap
+
+  elif [ "$2" = "sd" ]; then
+    opkg update
+
+     opkg -d sd install /sd/modules/wps/ipks/reaver-custom_1.6.6-5_mips_24kc.ipk
+     opkg -d sd install /sd/modules/wps/ipks/bully_1.4-1_mips_24kc.ipk
+     opkg -d sd install /sd/modules/wps/ipks/pixiewps-custom_1.4.2-3_mips_24kc.ipk
+     opkg -d sd install libpcap
+
+  fi
+  
+  if [ ! -f /usr/lib/libpcap.so ] && [ -f /usr/lib/libpcap.so.1.3 ]; then
+  	ln -s /usr/lib/libpcap.so /usr/lib/libpcap.so.1.3
+  fi
+
+  touch /etc/config/wps
+  echo "config wps 'module'" > /etc/config/wps
+
+  uci set wps.module.installed=1
+  uci commit wps.module.installed
+
+elif [ "$1" = "remove" ]; then
+    opkg remove reaver-custom
+    opkg remove bully
+    opkg remove pixiewps-custom
+    rm -rf /etc/config/wps
+fi
+
+rm /tmp/wps.progress
---
+++ wps/scripts/wps.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#2015 - Whistle Master
+
+
+
+MYTIME=`date +%s`
+MYCMD=`cat /tmp/wps.run`
+
+if [ "$1" = "start" ]; then
+	eval ${MYCMD}
+	rm -rf /tmp/wps.run
+        rm -rf /tmp/lazy.read
+elif [ "$1" = "stop" ]; then
+ 	killall -9 reaver
+	killall -9 bully
+
+	rm -rf /tmp/wps.run
+        rm -rf /tmp/lazy.read
+fi
