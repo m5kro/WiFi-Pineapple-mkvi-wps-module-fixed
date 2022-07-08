037faa2f438190f95213b8f6b4b1a17b5f5a15d9
kraken5 <44246711+kraken5@users.noreply.github.com>
2022-07-08 00:31:12 -0700

    Update dependencies.sh

--- wps/scripts/dependencies.sh
+++ wps/scripts/dependencies.sh
@@ -9,16 +9,15 @@ touch /tmp/wps.progress
 
 if [ "$1" = "install" ]; then
   if [ "$2" = "internal" ]; then
-	 opkg update
-
+     opkg update
      opkg install /pineapple/modules/wps/ipks/reaver-custom_1.6.6-5_mips_24kc.ipk
      opkg install /pineapple/modules/wps/ipks/bully_1.4-1_mips_24kc.ipk
      opkg install /pineapple/modules/wps/ipks/pixiewps-custom_1.4.2-3_mips_24kc.ipk
      opkg install libpcap
 
   elif [ "$2" = "sd" ]; then
-    opkg update
-
+     opkg update
+     ln -s /sd/modules/wps /pineapple/modules/
      opkg -d sd install /sd/modules/wps/ipks/reaver-custom_1.6.6-5_mips_24kc.ipk
      opkg -d sd install /sd/modules/wps/ipks/bully_1.4-1_mips_24kc.ipk
      opkg -d sd install /sd/modules/wps/ipks/pixiewps-custom_1.4.2-3_mips_24kc.ipk
